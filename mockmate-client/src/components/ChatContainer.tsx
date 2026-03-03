c00decc1eeb6a283dc17bfc1ebc7e14d3de74887
imanchalsingh <singhanchal159357@gmail.com>
2026-03-03 20:00:18 +0530

    fetch api

--- mockmate-client/src/components/ChatContainer.tsx
+++ mockmate-client/src/components/ChatContainer.tsx
@@ -21,16 +21,15 @@ export default function ChatContainer() {
 
         setMessages(prev => [...prev, userMessage]);
 
-        const res = await fetch(
-            "http://localhost:3000/api/interview/ask",
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ message: text })
-            }
-        );
+        const res = await fetch("http://localhost:5000/api/interview/ask", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                message: text,
+            }),
+        });
 
         const data = await res.json();
 
