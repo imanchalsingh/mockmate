18141cf7f4e0dc7007d936c99d4fb08684f21915
imanchalsingh <singhanchal159357@gmail.com>
2026-03-04 17:58:58 +0530

    added routes

--- mockmate-client/src/App.tsx
+++ mockmate-client/src/App.tsx
@@ -1,11 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Interview from './pages/Interview'
+import VoiceInterview from './pages/VoiceInterview'
+import HeroSection from './components/HeroSection'
 
 function App() {
-
   return (
     <div>
-      <Interview />
+      <Routes>
+        <Route path='/' element={<HeroSection />} />
+        <Route path='/chats-interview' element={<Interview />} />
+        <Route path='/voice-interview' element={<VoiceInterview />} />
+      </Routes>
     </div>
   )
 }
