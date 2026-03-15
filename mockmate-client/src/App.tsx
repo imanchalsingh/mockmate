69db7edb67d5b674689044422140055afa2ae7e3
imanchalsingh <singhanchal159357@gmail.com>
2026-03-15 13:27:56 +0530

    feat: update routing to set Login as the default landing page

--- mockmate-client/src/App.tsx
+++ mockmate-client/src/App.tsx
@@ -3,12 +3,16 @@ import './App.css'
 import Interview from './pages/Interview'
 import VoiceInterview from './pages/VoiceInterview'
 import HeroSection from './components/HeroSection'
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<HeroSection />} />
+        <Route path='/' element={<Login />} />
+        <Route path='/mockmate-home-page' element={<HeroSection />} />
+        <Route path='/register' element={<Register />} />
         <Route path='/chats-interview' element={<Interview />} />
         <Route path='/voice-interview' element={<VoiceInterview />} />
       </Routes>
