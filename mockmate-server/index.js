79caf3249314d1949f034514b25c48610fd26c97
imanchalsingh <singhanchal159357@gmail.com>
2026-03-15 13:26:12 +0530

    refactor: reorganize server setup and add MongoDB connection

--- mockmate-server/index.js
+++ mockmate-server/index.js
@@ -3,15 +3,21 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import interviewRouter from './src/routes/interview.js';
-
-const app = express();
-app.use(express.json());
+import { connectDB } from "./src/config/db.js";
+import authRoutes from "./src/routes/auth.js";
 
 const PORT = process.env.PORT || 5000;
+connectDB();
 
+const app = express();
 app.use(cors());
 
+app.use(express.json());
+
+
 app.use('/api/interview', interviewRouter);
+app.use("/api/auth", authRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`Mockmate server is running on port ${PORT}`);
