4baa51eb1390d3973e4a6ff0b741b8ff4db4fa3e
imanchalsingh <singhanchal159357@gmail.com>
2026-03-23 12:50:56 +0530

    feat: add middleware to parse JSON requests

--- mockmate-server/index.js
+++ mockmate-server/index.js
@@ -10,9 +10,9 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+app.use(express.json());
 app.use(cors());
 
-app.use(express.json());
 
 
 app.use('/api/interview', interviewRouter);
